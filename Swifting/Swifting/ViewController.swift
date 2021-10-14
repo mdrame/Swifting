be8bffa43264d35a62585dd8440799f324ef0bc2
mohammed <mdrame113@gmail.com>
2021-10-14 15:37:58 -0400

    init project

---
+++ Swifting/Swifting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swifting
+//
+//  Created by Mohammed Drame on 10/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
