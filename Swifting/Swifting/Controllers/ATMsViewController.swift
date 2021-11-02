847dac212f575258aa3686482c66068450725a5b
mohammed <mdrame113@gmail.com>
2021-11-02 02:32:26 -0400

    Favorite currency VC & TableView crated

--- Swifting/Swifting/Controllers/ATMsViewController.swift
+++ Swifting/Swifting/Controllers/ATMsViewController.swift
@@ -12,19 +12,13 @@ class ATMsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-        print("View didLoad")
-        // Do any additional setup after loading the view.
+        self.title = "ATM"
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
     
+    
+    
+    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Swifting/Swifting/Controllers/FavoritesVC.swift
+++ Swifting/Swifting/Controllers/FavoritesVC.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-class FavoritesVC: UIViewController {
+class FavoritesVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        self.title = "Favorite Currencies"
+        navigationController?.navigationBar.prefersLargeTitles = true
         // Do any additional setup after loading the view.
     }
+    
+    @IBOutlet weak var favoriteCurrenciesTableView: UITableView!
+    
+    // TablevView Delegates and Data source
+    
+    
+    // Tableview delegate and Data source
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //        return cryptoCurrencies.count
+//        print("Crypto Count", cryptoCurrencies.count)
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteCell.cellIdentifier, for: indexPath) as! FavoriteCell
+//        cell.updateCellUI(with: cryptoCurrencies[indexPath.row])
+//        cell.currencyCellDelegate = self
+//        cell.favorite_button.imageView?.image = cryptoCurrencies[indexPath.row].isFavorite ? UIImage(named: "yellowstar.png") : UIImage(named: "clearstar.png")
+//        print("Cell for row at method called")
+        //        cell.selectionStyle = .blue
+        return cell
+    }
+    
+    
+    
+    
     
 
 
