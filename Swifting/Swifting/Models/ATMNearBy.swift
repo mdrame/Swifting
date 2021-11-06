6cc1401b33b2bf47ec6641fec64534f50c9899e4
mohammed <mdrame113@gmail.com>
2021-11-06 02:05:23 -0400

    coinmap model created

--- Swifting/Swifting/Models/ATMNearBy.swift
+++ Swifting/Swifting/Models/ATMNearBy.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 
-struct ATMNearBy: Codable {
+struct ATMsNearBy: Codable {
     let venues: [Venue]
 }
 
@@ -20,3 +20,44 @@ struct Venue: Codable  {
     let geolocation_degrees : String
 }
 
+
+// MARK: - Welcome9
+struct Location: Codable {
+    let atm: ATM
+}
+
+// MARK: - Venue
+struct ATM: Codable {
+    let category, state: String
+    let fax: String
+    let updatedOn: Int
+    let logo: String
+    let name, country: String
+    let openingHours, logoURL: String
+    let nameASCII: String
+    let website: String
+    let venueDescription: String
+    let atmProviderName, coupon: String
+    let street, facebook: String
+    let location, atmOperatorName, couponExpiration: String
+    let id: Int
+    let houseno, twitter: String
+    let promotedUntil: String
+    let parking: String
+    let couponDescription, srcID: String
+    let city: String
+    let instagram, premiumUntil, dailyLimits: String
+    let lat: Double
+    let postcode, email: String
+    let createdOn: Int
+    let lon: Double
+    let phone: String
+    let coins: [String] // ANY ‼️
+    let user: User
+    let geolocationDegrees: String
+}
+
+// MARK: - User
+struct User: Codable {
+    let userhash: String
+}
